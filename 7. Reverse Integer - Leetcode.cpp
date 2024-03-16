b4c3c1070836ed7da46fa7c297d2f6c9b46b57b1
Tushar Vijay <97074208+tushar-vijay@users.noreply.github.com>
2024-03-16 11:50:17 +0530

    Create 7. Reverse Integer - Leetcode.cpp

---
+++ 7. Reverse Integer - Leetcode.cpp
@@ -0,0 +1,20 @@
+#include<limits.h>
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+            
+        while(x){
+            int digit = x%10;
+            if(rev > (INT_MAX)/10 || rev < (INT_MIN)/10){
+                return 0;
+            }
+            rev = (rev*10) + digit;
+            
+            x /= 10;
+        }
+        
+        return rev;
+    }
+};
