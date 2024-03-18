50b7db64b550ff4d4ba3580933b181db2b3a9aa9
Tushar Vijay <97074208+tushar-vijay@users.noreply.github.com>
2024-03-18 10:23:16 +0530

    Create 852. Peak Index in a Mountain Array - Leetcode.cpp

---
+++ 852. Peak Index in a Mountain Array - Leetcode.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s = 0, e = arr.size() - 1;
+        int mid = s + (e-s)/2;
+
+        while(s < e){
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid] > arr[mid-1] && arr[mid]<arr[mid+1]){
+                s = mid;
+            }
+            else{
+                e = mid;
+            }
+            mid = s + (e-s)/2;
+        }
+        return -1;
+    }
+};
