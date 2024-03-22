52cf29e4c235d8476f24f2ba82655b3f48e870e9
Tushar Vijay <97074208+tushar-vijay@users.noreply.github.com>
2024-03-22 22:04:21 +0530

    Create Binary Search - Coding Ninjas.cpp

---
+++ Binary Search - Coding Ninjas.cpp
@@ -0,0 +1,19 @@
+int search(vector<int> &nums, int target) {
+
+    int n = nums.size();
+    int s = 0, e = n - 1;
+    int mid = s + (e-s)/2;
+
+    while(s <= e){
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] > target)
+            e = mid - 1;
+        else
+            s = mid + 1;
+        
+        mid = s + (e-s)/2;
+    }
+
+    return -1;
+}
