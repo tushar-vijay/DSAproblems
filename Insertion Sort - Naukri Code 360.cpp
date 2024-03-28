3b1df0f4d2a95f21e4ccc1d4c43a8ec10c38d2fd
Tushar Vijay <97074208+tushar-vijay@users.noreply.github.com>
2024-03-28 19:34:03 +0530

    Create Insertion Sort - Naukri Code 360.cpp

---
+++ Insertion Sort - Naukri Code 360.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+void insertionSort(int n, vector<int> &arr) {
+
+    int temp;
+
+    for(int i=1; i<n; i++){
+
+        temp = arr[i];
+        int j = i-1;
+
+        while(j >= 0){
+
+            if(arr[j] > temp){
+                arr[j+1] = arr[j];
+            }
+            else
+                break;
+            
+            j--;
+        }
+
+        arr[j+1] = temp;
+    }
+}
